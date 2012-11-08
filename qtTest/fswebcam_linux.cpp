5c850a2180b6375985aefceb7f25cfe18e2f94f8
Francis Engelmann <francis@AsusKiste.(none)>
2012-11-08 19:59:44 +0100

    bazinga

--- qtTest/fswebcam_linux.cpp
+++ qtTest/fswebcam_linux.cpp
@@ -3,6 +3,7 @@
  */
 
 #include "fswebcam.h"
+#include <QProcess>
 
 FSWebCam::FSWebCam()
 {
@@ -17,6 +18,7 @@ FSWebCam::~FSWebCam()
 
 QList<FSWebCamInfo> FSWebCam::enumerate()
 {
+    QList<FSWebCamInfo> list;
     QString program = "lsusb";
     QStringList arguments;
     // logitech c270
@@ -25,9 +27,18 @@ QList<FSWebCamInfo> FSWebCam::enumerate()
 
     QProcess proc;
     //proc.start("lsusb",QStringList() << "-d" << "046d:0825");
-    proc.start("ls",QStringList() << "/dev/video*");
-    if(!proc.waitForFinished()) return;
-
+    proc.start("ls",QStringList() << "/dev");
+    if(!proc.waitForFinished()) return list;
     QByteArray result = proc.readAll();
-    qDebug(result);
+    QList<QByteArray> results = result.split('\n');
+    foreach(QByteArray device, results){
+        if(device.startsWith("video")){
+            FSWebCamInfo cam;
+            cam.portName = "/dev/";
+            cam.portName.append(device);
+            list.append(cam);
+            //qDebug() << "Result: " << path;
+        }
+    }
+    return list;
 }
