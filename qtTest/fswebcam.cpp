cfd458ab3cc65da4f4bc77288a4a56bc5286dbfb
Francis Engelmann <francis.engelmann@rwth-aachen.de>
2012-11-08 16:40:25 +0100

    added fswebcam class

---
+++ qtTest/fswebcam.cpp
@@ -0,0 +1,5 @@
+#include "fswebcam.h"
+
+FSWebCam::FSWebCam()
+{
+}
---
+++ qtTest/fswebcam.h
@@ -0,0 +1,15 @@
+/**
+ * This class controls the webcam and provides functionality
+ * such as listing all available cams or grabbing images.
+ */
+
+#ifndef FSWEBCAM_H
+#define FSWEBCAM_H
+
+class FSWebCam
+{
+public:
+    FSWebCam();
+};
+
+#endif // FSWEBCAM_H
