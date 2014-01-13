5c10ad371f9fab949fd31b756933fec2395f09cc
René Bohne <rene.bohne@gmail.com>
2014-01-13 14:29:23 +0100

    set default stack size to 32MB. Maybe fixes #5

--- qtTest/main.cpp
+++ qtTest/main.cpp
@@ -3,9 +3,29 @@
 
 int main(int argc, char *argv[])
 {
+
+    const rlim_t kStackSize = 32L * 1024L * 1024L;   // min stack size = 32 Mb
+    struct rlimit rl;
+    int result;
+
+    result = getrlimit(RLIMIT_STACK, &rl);
+    if (result == 0)
+    {
+        if (rl.rlim_cur < kStackSize)
+        {
+            rl.rlim_cur = kStackSize;
+            result = setrlimit(RLIMIT_STACK, &rl);
+            if (result != 0)
+            {
+                fprintf(stderr, "setrlimit returned result = %d\n", result);
+            }
+        }
+    }
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-    
+
     return a.exec();
+
 }
